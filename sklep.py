8ca03002c6b5801f0585e0d6fc159f6def9f5e37
Aleksandra Przywrzej <przepjurka@gmail.com>
2020-08-10 18:06:56 +0200

    suma zbiorow by policzyc sume produktow

--- sklep.py
+++ sklep.py
@@ -1,7 +1,8 @@
 shopping_list = {'Piekarnia' :  ['Chleb', 'Pączek', 'Bułki'], \
 'Warzywniak' :  ['Marchew', 'Seler', 'Rukola']}
 for  sklep in shopping_list:
-    print(f'Idę do  {sklep} kupic następujące rzeczy {shopping_list[sklep]}
-    Piekarnia =set{}
-    Warzywniak=set{}
+    print(f'Idę do  {sklep} kupic następujące rzeczy {shopping_list[sklep]}')
+Piekarnia =set()
+Warzywniak=set()
+sum_set=Piekarnia|Warzywniak
 print(f'W sumie kupuje {sum_set} produktów.')
