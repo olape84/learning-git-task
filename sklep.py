9e6c357e4d202449a6e889271db3419d8803fc1c
Aleksandra Przywrzej <przepjurka@gmail.com>
2020-08-18 16:51:08 +0200

    dzialajaca suma produktow

--- sklep.py
+++ sklep.py
@@ -2,7 +2,5 @@ shopping_list = {'Piekarnia' :  ['Chleb', 'Pączek', 'Bułki'], \
 'Warzywniak' :  ['Marchew', 'Seler', 'Rukola']}
 for  sklep in shopping_list:
     print(f'Idę do  {sklep} kupic następujące rzeczy {shopping_list[sklep]}')
-Piekarnia =set()
-Warzywniak=set()
-sum_set=Piekarnia|Warzywniak
-print(f'W sumie kupuje {sum_set} produktów.')
+print(f'W sumie kupuje {sum(len(shopping_list[sklep]) for products in shopping_list.values()} produktow')
+ 
